cb67fd2bbb66195f89e5612ec95b9e7e750b1727
S. Espinet <s.lou.espinet@gmail.com>
2022-03-25 10:03:28 -0700

    add content to HomeController, HomeIndex files

--- SweetAndSavory/Controllers/HomeController.cs
+++ SweetAndSavory/Controllers/HomeController.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SweetAndSavory.Models;
 
 namespace SweetAndSavory.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
+    private readonly SweetAndSavoryContext _db;
+
+    public HomeController(SweetAndSavoryContext db)
+    {
+      _db = db;
     }
-  }
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        ViewBag.PageTitle = "Pierre's Sweet and Savory Treats";
+        var model = new IndexDisplay();
+        model.Flavors = _db.Flavors.ToList();
+        model.Treats = _db.Treats.ToList();
+        return View(model);
+      }
+    }
 }
